feebb2a525b37564365e84a5ba665b606a0e9221
Park-Seaweed <qkralsgur721@gmail.com>
2023-03-11 22:38:28 +0900

    FEAT: 리엑트 프로젝트 생성 및 라우터 설정

---
+++ frontend/src/pages/AddPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const AddPage = () => {
+    return (
+        <div>
+            안녕11111111
+        </div>
+    )
+}
+
+export default AddPage
---
+++ frontend/src/pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const MainPage = () => {
+    return (
+        <div>
+            안녕
+        </div>
+    )
+}
+
+export default MainPage
