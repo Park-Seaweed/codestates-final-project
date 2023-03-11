47d7d0692232ec95191733390b3248ec613b140a
Park-Seaweed <qkralsgur721@gmail.com>
2023-03-11 23:15:44 +0900

    FEAT: 메인 페이지 버튼 추가

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,11 +1,37 @@
 import React from "react";
 import MainTable from "../components/MainTable";
+import styled from 'styled-components'
 
 
 const MainPage = () => {
     return (
-        <MainTable />
+        <>
+            <MainTable />
+            <StBottom>
+                <StButton type='button' >
+                    글쓰기
+                </StButton>
+            </StBottom>
+        </>
+
     )
 }
 
 export default MainPage
+
+
+const StButton = styled.button`
+    border: none;
+    padding: 10px 20px;
+    background-color: #ffbc3f;
+    color: white;
+    font-weight: bold;
+    border-radius: 15px;
+    box-shadow: rgba(0, 0, 0, 0.2) 0 3px 5px -1px,
+    rgba(0, 0, 0, 0.14) 0 6px 10px 0, rgba(0, 0, 0, 0.12) 0 1px 18px 0;
+`;
+
+const StBottom = styled.div`
+    width: 100%;
+    margin-top: 30px;
+`;
