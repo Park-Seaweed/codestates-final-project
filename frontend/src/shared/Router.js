3c0002115a8b126bb236ccecbcec69e289fac5dc
Park-Seaweed <qkralsgur721@gmail.com>
2023-03-19 01:56:54 +0900

    FEAT: 로그인 안 한 유저 메인페이지 접근 제한

--- frontend/src/shared/Router.js
+++ frontend/src/shared/Router.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Route, Routes, useNavigate, useLocation } from 'react-router-dom';
+
+import SignInPage from '../pages/SignInPage';
+import SignUpPage from '../pages/SignUpPage';
 import AddPage from '../pages/AddPage';
 import DetailPage from '../pages/DetailPage';
 import MainPage from '../pages/MainPage';
-import SignInPage from '../pages/SignInPage';
-import SignUpPage from '../pages/SignUpPage';
 import VerifyPage from '../pages/VerifyPage';
 
 const Router = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const isLoginRequired =
+      !localStorage.getItem('accessToken') &&
+      !location.pathname.startsWith('/signup') &&
+      !location.pathname.startsWith('/verify');
+
+    if (isLoginRequired) {
+      navigate('/signin');
+    }
+  }, [location.pathname, navigate]);
   return (
     <Routes>
       <Route path='/' element={<MainPage />} />
