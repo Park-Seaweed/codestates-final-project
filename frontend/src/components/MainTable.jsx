6b6b949528ba9c237ec04afc875b3402900d5e8b
Park-Seaweed <qkralsgur721@gmail.com>
2023-03-19 01:42:31 +0900

    STYLE: 메인 테이블 스타일 수정

--- frontend/src/components/MainTable.jsx
+++ frontend/src/components/MainTable.jsx
@@ -1,82 +1,86 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
 const MainTable = ({ data }) => {
-    const navigate = useNavigate()
-    console.log(data)
-    if (data) {
-        return (
-            <div>
-                <StTable>
-                    <thead>
-                        <tr>
-                            <StTh>제목</StTh>
-                            <StTh>작성자</StTh>
-                            <StTh>작성 시간</StTh>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.map((item) => {
-                            return (
-                                <tr
-                                    onClick={() =>
+  const navigate = useNavigate();
 
-                                        navigate(`/detail/${item.id}`)
-                                    }
-                                    key={item.id}
-                                >
-                                    <StTd>{item.title}</StTd>
-                                    <StTd>{item.id}</StTd>
-                                    <StTd>{item.created_at}</StTd>
-                                </tr>
-                            );
-                        })}
-
-                    </tbody>
-                </StTable>
-            </div >
-        );
-    };
-}
-
+  if (data) {
+    return (
+      <>
+        <StTable>
+          <thead>
+            <tr>
+              <StTh>번호</StTh>
+              <StTh>제목</StTh>
+              <StTh>작성자</StTh>
+              <StTh>작성 시간</StTh>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => {
+              return (
+                <StTr
+                  key={item.id}
+                  onClick={() => navigate(`/detail/${item.id}`)}
+                >
+                  <StTd>{item.id}</StTd>
+                  <StTd>{item.title}</StTd>
+                  <StTd>{item.user_nickname}</StTd>
+                  <StTd>{item.created_at}</StTd>
+                </StTr>
+              );
+            })}
+          </tbody>
+        </StTable>
+      </>
+    );
+  }
+};
 
 export default MainTable;
 
-
 const StTable = styled.table`
-    border-collapse: collapse;
-    margin: 20px 0;
-    text-align: center;
-    border-radius: 24px;
-    box-shadow: rgba(0, 0, 0, 0.2) 0 3px 5px -1px,
+  border-collapse: collapse;
+  margin: 0 0 20px 0;
+  text-align: center;
+  border-radius: 24px;
+  box-shadow: rgba(0, 0, 0, 0.2) 0 3px 5px -1px,
     rgba(0, 0, 0, 0.14) 0 6px 10px 0, rgba(0, 0, 0, 0.12) 0 1px 18px 0;
-    overflow: hidden;
+  overflow: hidden;
+  width: 850px;
 `;
 
 const StTh = styled.th`
-    border-bottom: 1px solid #ddd;
-    background-color: #ffbc3f;
-    font-weight: bold;
-    padding: 1rem;
-    color: white;
+  border-bottom: 1px solid #ddd;
+  background-color: #ffbc3f;
+  font-weight: bold;
+  padding: 1rem;
+  color: white;
 
-    &:first-child {
-    width: 400px;
-    }
-    &:nth-child(2) {
+  &:first-child {
+    width: 10px;
+  }
+  &:nth-child(2) {
+    width: 450px;
+  }
+  &:nth-child(3) {
     width: 100px;
-    }
-    &:last-child {
-    width: 120px;
-    }
+  }
+  &:last-child {
+    width: auto;
+  }
+`;
+
+const StTr = styled.tr`
+  height: 55px;
 `;
 
 const StTd = styled.td`
-    border-top: 1px solid #ddd;
-    padding: 1rem 2rem;
+  border-top: 1px solid #ddd;
+  padding: 0 2rem;
 
-    &:first-child {
+  &:nth-child(2) {
     text-align: left;
-    }
+  }
 `;
