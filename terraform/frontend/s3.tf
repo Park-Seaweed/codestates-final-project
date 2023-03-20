c5de3e1943b70c86d451c8a1f0ebecdfab9dd09d
gangdongguri <rhkdgnsvk2@naver.com>
2023-03-20 18:32:35 +0900

    MODI: s3 ACL 편집 - block_public_policy

--- terraform/frontend/s3.tf
+++ terraform/frontend/s3.tf
@@ -27,7 +27,7 @@ resource "aws_s3_bucket_public_access_block" "s3_public_access" {
   bucket = aws_s3_bucket.demo-tf-test-bucket.id
 
   block_public_acls       = true
-  block_public_policy     = true
+  block_public_policy     = false
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
