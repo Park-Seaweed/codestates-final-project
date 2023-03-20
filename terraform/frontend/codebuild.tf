6f2f0e42a5c930a098dd58943b980b3870ec26ee
si946bi <minji903211k@naver.com>
2023-03-19 18:06:36 -0700

    FEAT: codepipeline terraform 리소스 추가

---
+++ terraform/frontend/codebuild.tf
@@ -0,0 +1,99 @@
+resource "aws_iam_role" "codebuild_role" {
+  name = "terraform-codebuild"
+  assume_role_policy = <<EOF
+{
+   "Version": "2012-10-17",
+   "Statement": [
+      {
+         "Effect": "Allow",
+         "Principal": {
+            "Service": "codebuild.amazonaws.com"
+         },
+         "Action": "sts:AssumeRole"
+      }
+   ]
+}
+EOF
+}
+resource "aws_iam_policy" "codebuild_policy" {
+  description = "CodeBuild Execution Policy"
+  policy      = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    },
+    {
+      "Action": [
+        "s3:GetObject", "s3:GetObjectVersion", "s3:PutObject", "s3:GetBucketAcl", "s3:GetBucketLocation", "codestar-connections:UseConnection"
+      ],
+      "Effect": "Allow",
+     resources = [
+      aws_s3_bucket.artifact_bucket.arn,
+      aws_s3_bucket.demo-tf-test-bucket.arn,
+      "${aws_s3_bucket.artifact_bucket.arn}/*"
+    ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codebuild:CreateReportGroup",
+        "codebuild:CreateReport",
+        "codebuild:UpdateReport",
+        "codebuild:BatchPutTestCases",
+        "codebuild:BatchPutCodeCoverages"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codestar-connections:CreateConnection",
+        "codestar-connections:GetConnection",
+        "codestar-connections:DeleteConnection",
+        "codestar-connections:ListConnections"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild-attach" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = aws_iam_policy.codebuild_policy.arn
+}
+
+resource "aws_codebuild_project" "codebuild" {
+  name         = "codebuild-${var.source_repo_name}-${var.source_repo_branch}"
+  service_role = aws_iam_role.codebuild_role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+  
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:5.0"
+    type                        = "LINUX_CONTAINER"
+    privileged_mode             = true
+    image_pull_credentials_type = "CODEBUILD"
+
+    environment_variable {
+      name  = var.react_env
+      value = var.backend_endpoint
+    }
+  }
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = <<BUILDSPEC
+${file("buildspec.yml")}
+BUILDSPEC
+  }
+}
--- terraform/frontend/s3.tf
+++ terraform/frontend/s3.tf
@@ -1,7 +1,58 @@
-resource "aws_s3_bucket" "test_bucket" {
-  bucket = "terraform-test-alsgur"
-  tags = {
-    Name = "test"
+resource "aws_s3_bucket" "demo-tf-test-bucket" {
+  bucket = "demo-tf-test-bucket"
+  acl    = "private"
 
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+    }
+
+  versioning {
+    enabled = true
+  }
+}
+
+# aws_s3_bucket_public_access_block
+resource "aws_s3_bucket_public_access_block" "s3_public_access" {
+  bucket = aws_s3_bucket.demo-tf-test-bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
+#aws_s3_bucket_policy
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.demo-tf-test-bucket.id
+  policy = jsonencode({
+    "Version": "2008-10-17",
+    "Id": "PolicyForCloudFrontPrivateContent",
+    "Statement": [
+      {
+        "Sid": "AllowCloudFrontServicePrincipal",
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "cloudfront.amazonaws.com"
+        },
+        "Action": "s3:GetObject",
+        "Resource": "${aws_s3_bucket.demo-tf-test-bucket.arn}/*",
+        "Condition": {
+          "StringEquals": {
+            "AWS:SourceArn": "${aws_cloudfront_distribution.s3_distribution.arn}"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# aws_s3_bucket_server_side_encryption_configuration
+resource "aws_s3_bucket_server_side_encryption_configuration" "key_encryption" {
+  bucket = aws_s3_bucket.demo-tf-test-bucket.id
+
+  rule {
+    bucket_key_enabled = true
   }
 }
