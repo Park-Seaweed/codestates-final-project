7c6ded477a42f9a524584c065d5e6b623d43dd42
Park-Seaweed <qkralsgur721@gmail.com>
2023-03-21 11:21:56 +0900

    TEST: ecs 배포 테스트

--- backend/routes/root.js
+++ backend/routes/root.js
@@ -2,6 +2,6 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    reply.code(200).send({sever: "okkkkkkkkk"})
+    reply.code(200).send({sever: "okkkkkkkkk11111111"})
   })
 }
