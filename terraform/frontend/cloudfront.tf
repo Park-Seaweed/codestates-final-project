e4cb5a91722824add05c241b203d49e140e3bc32
gangdongguri <rhkdgnsvk2@naver.com>
2023-03-21 16:08:54 +0900

    MODI: custom_error_response 겹따옴표 제거

--- terraform/frontend/cloudfront.tf
+++ terraform/frontend/cloudfront.tf
@@ -25,14 +25,14 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   custom_error_response = [
     {
-      "error_code"          = 403
-      "response_code"       = 200
-      "response_page_path"  = "/index.html"
+      error_code          = 403
+      response_code       = 200
+      response_page_path  = "/index.html"
     },
     {
-      "error_code"          = 404
-      "response_code"       = 200
-      "response_page_path"  = "/index.html"
+      error_code          = 404
+      response_code       = 200
+      response_page_path  = "/index.html"
     }
   ]
 
