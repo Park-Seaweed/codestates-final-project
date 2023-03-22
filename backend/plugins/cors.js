8712eb8152a060385975ccd354413a66ba7ebbc2
Park-Seaweed <qkralsgur721@gmail.com>
2023-03-22 14:18:28 +0900

    TEST: origin 테스트

--- backend/plugins/cors.js
+++ backend/plugins/cors.js
@@ -3,5 +3,7 @@
 const fp = require('fastify-plugin');
 
 module.exports = fp(async function (fastify, opts) {
-    fastify.register(require('@fastify/cors'));
+    fastify.register(require('@fastify/cors'),{
+        origin: 'https://www.devops03-gg.click/*'
+    });
 });
