802faddf5b2c142089a5a6249b57b66e15c31d68
Park-Seaweed <qkralsgur721@gmail.com>
2023-03-22 14:33:37 +0900

    TEST: origin 삭제

--- backend/plugins/cors.js
+++ backend/plugins/cors.js
@@ -3,7 +3,5 @@
 const fp = require('fastify-plugin');
 
 module.exports = fp(async function (fastify, opts) {
-    fastify.register(require('@fastify/cors'),{
-        origin: 'https://www.devops03-gg.click'
-    });
+    fastify.register(require('@fastify/cors'));
 });
